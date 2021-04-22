17912f32e27be4e074b3eeaa8def46afd6d0a7ae
mpsre <mp7652@zebra.com>
2021-04-22 14:51:09 -0500

    --- Auto-Git Commit ---

---
+++ .history/vars/nodejs_20210422145102.groovy
@@ -0,0 +1,3 @@
+def call(String COMPONENT_NAME) {
+
+}
--- .history/vars/nodejs_20210422145103.groovy
+++ .history/vars/nodejs_20210422145103.groovy
@@ -1,9 +1,9 @@
 def call(String COMPONENT_NAME) {
 pipeline {
     agent any 
-      parameters {
-                string(name: 'PERSON', defaultValue: 'Mr Jenkins', description: 'Who should I say hello to?')
-            }
+        parameters {
+            string(name: 'PERSON', defaultValue: 'Mr Jenkins', description: 'Who should I say hello to?')
+        }
     stages {
         stage(''){
             steps {
