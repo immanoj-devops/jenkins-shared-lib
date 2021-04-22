3f412dcc116de9cc40b84dc5b64719a9e9f37c63
mpsre <mp7652@zebra.com>
2021-04-22 15:09:50 -0500

    --- Auto-Git Commit ---

---
+++ .history/vars/nodejs_20210422150944.groovy
@@ -0,0 +1,14 @@
+def call(String COMPONENT_NAME) {
+pipeline {
+    agent any
+    stages {
+        stage('Sonar Scan') {
+            steps {
+               sh "sonar-scanner  -Dsonar.projectKey=${COMPONENT_NAME} -Dsonar.sources=. -Dsonar.host.url=http://172.31.74.214:9000 -Dsonar.login=2f7e9cbf5079973a6f2258d0676c937738515ef7"
+
+               
+                }
+            }
+        }
+    }
+}
