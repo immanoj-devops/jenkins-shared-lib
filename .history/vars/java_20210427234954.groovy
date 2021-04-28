5b1ec8ceac64e11b780014ab0fa4a1f47bb0509e
Manoj Pachigolla <mp7652@zebra.com>
2021-04-27 23:50:03 -0500

    --- Auto-Git Commit ---

--- .history/vars/java_20210427234954.groovy
+++ .history/vars/java_20210427234954.groovy
@@ -2,7 +2,7 @@ def call(String COMPONENT_NAME) {
 pipeline {
     agent any
     stages {
-        stage('Compiling the Code ') {
+        stage('Compiling the Code Scan') {
             steps {
 
                sh "sonar-scanner  -Dsonar.projectKey=${COMPONENT_NAME} -Dsonar.sources=. -Dsonar.host.url=http://172.31.74.214:9000 -Dsonar.login=2f7e9cbf5079973a6f2258d0676c937738515ef7"             
